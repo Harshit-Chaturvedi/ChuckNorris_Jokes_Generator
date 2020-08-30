c0e5b0b65362bfb7d0cb00fd4d994a634b9d2b15
Harshit Chaturvedi <harshitchaturvedi1996@gmail.com>
2020-08-30 21:16:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// Event Listner
+document.querySelector(".get-jokes").addEventListener("click", getJokes);
+
+
+// Get Jokes 
+function getJokes(e) {
+    e.preventDefault();
+
+    const number = document.querySelector("input[type='number']").value;
+
+    // Initiate xhr
+    const xhr = new XMLHttpRequest();
+
+    // Configure
+    xhr.open("GET", `http://api.icndb.com/jokes/random/${number}`, true);
+
+
+
+    // Onload
+
+    xhr.onload = function () {
+
+        if (this.status === 200) {
+            const response = JSON.parse(this.response);
+
+            let output = "";
+            response.value.forEach(function (jokes) {
+                output += `
+                <li>${jokes.joke}</li>
+                `
+            });
+            document.querySelector("#number").value = "";
+            document.querySelector("#jokes div").style.display = "block";
+            document.querySelector(".jokes").innerHTML = output;
+        }
+    }
+    // Send Request
+    xhr.send()
+}
